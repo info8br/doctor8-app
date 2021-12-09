6b8d28f593ac6a66b7f29c260f569e5657c6491b
mehfius <matheus.ferraz@gmail.com>
2021-12-09 21:16:33 +0000

    botoes layout e mobile check

--- import/js/getMobile.js
+++ import/js/getMobile.js
@@ -1,9 +1,3 @@
-function contact(){
-
-window.open("https://api.whatsapp.com/send?phone=5531971720053&text=contato%20chat","_blank")
-
-}
-
 function getMobile(){
 
   if (window.innerWidth <= 480) {
@@ -17,6 +11,3 @@ function getMobile(){
   }
 
 }
-
-
-
