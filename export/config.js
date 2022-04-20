15b7d904a9ea8c42add2f556f7ecf06497b5750a
mehfius <matheus.ferraz@gmail.com>
2022-04-20 14:38:33 +0000

    supabase url

--- export/config.js
+++ export/config.js
@@ -6,6 +6,9 @@ localStorage.setItem("newusers", "1");
 localStorage.setItem("url", "https://supabase.mehfius.repl.co");
 
 
-// Supabasekey
+// Supabase
 
 localStorage.setItem("supabasekey", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTk1OTgyMSwiZXhwIjoxOTUxNTM1ODIxfQ.zVt5O10PXhNSp5VjdNWmZUOg8li69TA1X2wxlQ0JLqQ");
+
+localStorage.setItem("supabaseurl", "https://fozemduyrmnxgkfmzpnh.supabase.co/");
+
