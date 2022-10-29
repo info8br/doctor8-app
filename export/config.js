aaeabe2eca6351f46597d73fcfbe023824cab1a9
Matheus Ferraz <matheus.ferraz@gmail.com>
2022-10-29 17:25:16 +0000

    Dev to Production

--- export/config.js
+++ export/config.js
@@ -6,9 +6,14 @@ localStorage.setItem("newusers", "1");
 localStorage.setItem("url", "https://supabase.mehfius.repl.co");
 
 
-// Supabase
+// Supabase Dev
 
-localStorage.setItem("supabasekey", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTk1OTgyMSwiZXhwIjoxOTUxNTM1ODIxfQ.zVt5O10PXhNSp5VjdNWmZUOg8li69TA1X2wxlQ0JLqQ");
+/* localStorage.setItem("supabasekey", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTk1OTgyMSwiZXhwIjoxOTUxNTM1ODIxfQ.zVt5O10PXhNSp5VjdNWmZUOg8li69TA1X2wxlQ0JLqQ");
 
-localStorage.setItem("supabaseurl", "https://fozemduyrmnxgkfmzpnh.supabase.co/");
+localStorage.setItem("supabaseurl", "https://fozemduyrmnxgkfmzpnh.supabase.co/"); */
 
+// Supabase Production
+
+localStorage.setItem("supabasekey", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmbGh1cXF6am1na2Roamd4em5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE5MDE1NzIsImV4cCI6MTk3NzQ3NzU3Mn0.h8KOM1CCXPY80ImmmsrLmGp0Wib0z8C80KNFFGjzcn8");
+
+localStorage.setItem("supabaseurl", "https://vflhuqqzjmgkdhjgxzni.supabase.co/");
